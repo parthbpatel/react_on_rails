c003112ca1db883e3c0f9b2f7cb0561dfc3cdf91
parthbpatel <parthtech3010@gmail.com>
2020-08-01 17:51:27 +0530

    add faker gem, add seed data to seeds.rb - rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = Faker::Internet.email
+  User.create(f_name: first_name, l_name: last_name, email: email)
+end
